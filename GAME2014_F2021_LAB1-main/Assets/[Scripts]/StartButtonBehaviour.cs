1f0cc5062b4c389d1484a0a3c52834f4af4415ea
Amer Ali <ameralizee93@gmail.com>
2021-09-22 15:35:34 -0400

    Added Initial files

---
+++ GAME2014_F2021_LAB1-main/Assets/[Scripts]/StartButtonBehaviour.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartButtonBehaviour : MonoBehaviour
+{
+    public void OnStartPressed()
+    {
+        Debug.Log("Start Button Pressed");
+    }
+}
